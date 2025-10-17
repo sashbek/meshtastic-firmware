030c4ab2fe5039cb5014293b46c22436d439785c
unknown <sasha.bekoev@gmail.com>
2025-10-18 00:03:39 +0300

    fix: not working

--- src/modules/PongModule.cpp
+++ src/modules/PongModule.cpp
@@ -16,19 +16,8 @@ ProcessMessage PongModule::handleReceived(const meshtastic_MeshPacket &mp)
   reply->to = NODENUM_BROADCAST;
   reply->want_ack = true;
 
-  bool has_name = false;
-  String name = "";
-  meshtastic_NodeInfoLite* node_from = nodeDB->getMeshNode(mp.from);
-
-  if (node_from != nullptr
-    && node_from->has_user
-  )
-  {
-    name = String(node_from->user.short_name);
-    if (mp.to != NODENUM_BROADCAST) {
-      reply->to = mp.from;
-    }
-    has_name = true;
+  if (mp.to != NODENUM_BROADCAST) {
+    reply->to = mp.from;
   }
 
   if (mp.hop_start == mp.hop_limit)
